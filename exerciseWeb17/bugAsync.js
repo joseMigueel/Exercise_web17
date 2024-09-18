6291be35d118279def0d010582e2938ffa5ffb34
joseMigueel <josemiguemarre99@gmail.com>
2024-09-18 16:36:18 +0100

    ejercicio 5 hecho

---
+++ exerciseWeb17/bugAsync.js
@@ -0,0 +1,21 @@
+// Este programa simula una llamada asincrónica para obtener un usuario
+function obtenerUsuario(id) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            if (id === 1) {
+                resolve({ id: 1, nombre: 'John Doe' }); 
+            }else {
+                resolve(null); 
+            }
+        }, 2000);
+    });
+}
+
+
+async function main() {
+    const usuario = await obtenerUsuario(1); 
+    console.log(usuario); 
+}
+main();
+
+
